9c6653316f57c3d098a7cb1009d4be34b43adb1f
Hisyamuel <hisyam4633@gmail.com>
2024-09-24 20:04:24 -0700

    konfigurasi menengah project mini coding

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // Ini Javascript 2
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+      alert("Tidak boleh ada yang kosong");
+      
+      return false;  
+    }
+
+    setSenderUI(name, birthDate, gender, messages);
+
+    return false;
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
